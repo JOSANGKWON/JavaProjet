ff5d383331658e601a115cc4256413ada0424e33
상권 <145637253+JOSANGKWON@users.noreply.github.com>
2023-09-21 15:27:40 +0900

    git

---
+++ git_pra1.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package gitpractice;
+
+/**
+ * @author JSK
+ *
+ */
+public class git_pra1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int a = 300;
+		  int b = 200;
+		  
+		  int result = a+b;
+		  
+		  System.out.println(result);
+				  
+	}
+
+}
